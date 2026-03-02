c3c3ab6951ef542615d9829b83624fbd0d80212f
John Bradshaw <124829389+Red-Rockwell@users.noreply.github.com>
2026-03-02 00:08:26 +0000

    Add files via upload
    Manually added files.

---
+++ AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by John Bradshaw on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
